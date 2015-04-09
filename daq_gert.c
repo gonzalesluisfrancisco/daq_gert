d14b7c5ff897ff1aae876c213eb2de5b172c24c2
Fred Brooks <nsaspook@nsaspook.com>
2015-04-09 13:29:11 -0700

    source update

--- daq_gert.c
+++ daq_gert.c
@@ -279,12 +279,9 @@ static struct pic_platform_data pic_info_pic18 = {
 /* driver hardware numbers */
 #define NUM_DIO_CHAN  17
 #define NUM_DIO_CHAN_REV2       21
-#define NUM_DIO_CHAN_REV3       21
+#define NUM_DIO_CHAN_REV3       30
 #define NUM_DIO_OUTPUTS 8
 #define DIO_PINS_DEFAULT        0xff
-
-#define INP_GPIO(g) *(gpio+((g)/10)) &= ~(7<<(((g)%10)*3))
-#define SET_GPIO_ALT(g,a) *(gpio+(((g)/10))) |= (((a)<=3?(a)+4:(a)==4?3:2)<<(((g)%10)*3))
 
 /* Locals to hold pointers to the hardware */
 
@@ -315,104 +312,177 @@ static const struct comedi_lrange daqgert_ao_range = {1,
 
 /*
  Doing it the Arduino way with lookup tables...
-      Yes, it's probably more innefficient than all the bit-twidling, but it
+      Yes, it's probably more inefficient than all the bit-twidling, but it
       does tend to make it all a bit clearer. At least to me!
 
  pinToGpio:
       Take a Wiring pin (0 through X) and re-map it to the BCM_GPIO pin
       Cope for 2 different board revisions here
  */
-static const int *pinToGpio;
+static int *pinToGpio;
 
-/* From the Original Wiki - GPIO 0 through 7 */
-static const int pinToGpioR1 [64] = {
-    17, 18, 21, 22, 23, 24, 25, 4,
-    0, 1, /* I2C  - SDA0, SCL0            wpi  8 -  9 */
-    8, 7, /* SPI  - CE1, CE0              wpi 10 - 11 */
-    10, 9, 11, /* SPI  - MOSI, MISO, SCLK      wpi 12 - 14 */
-    14, 15, /* UART - Tx, Rx                wpi 15 - 16 */
+static int pinToGpioR1 [64] =
+{
+  17, 18, 21, 22, 23, 24, 25, 4,        // From the Original Wiki - GPIO 0 through 7:   wpi  0 -  7
+   0,  1,                               // I2C  - SDA1, SCL1                            wpi  8 -  9
+   8,  7,                               // SPI  - CE1, CE0                              wpi 10 - 11
+  10,  9, 11,                           // SPI  - MOSI, MISO, SCLK                      wpi 12 - 14
+  14, 15,                               // UART - Tx, Rx                                wpi 15 - 16
 
-    /* Padding: */
+// Padding:
 
-    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, /* ... 31 */
-    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, /* . 47 */
-    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, /* ..63 */
-};
+      -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,       // ... 31
+  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,       // ... 47
+  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,       // ... 63
+} ;
 
-/* From the Original Wiki - GPIO 0 through 7:   wpi  0 -  7 */
-static const int pinToGpioR2 [64] = {
-    17, 18, 27, 22, 23, 24, 25, 4,
-    2, 3, /* I2C  - SDA0, SCL0            wpi  8 -  9 */
-    8, 7, /* SPI  - CE1, CE0              wpi 10 - 11 */
-    10, 9, 11, /* SPI  - MOSI, MISO, SCLK      wpi 12 - 14 */
-    14, 15, /* UART - Tx, Rx                wpi 15 - 16 */
-    28, 29, 30, 31, /* New GPIOs 8 though 11        wpi 17 - 20 */
-    5, 6, 12, 13, 16, 19, 20, 21, 26, /* Rev 3 New GPIOs */
+// Revision 2:
 
-    /* Padding: */
+static int pinToGpioR2 [64] =
+{
+  17, 18, 27, 22, 23, 24, 25, 4,        // From the Original Wiki - GPIO 0 through 7:   wpi  0 -  7
+   2,  3,                               // I2C  - SDA0, SCL0                            wpi  8 -  9
+   8,  7,                               // SPI  - CE1, CE0                              wpi 10 - 11
+  10,  9, 11,                           // SPI  - MOSI, MISO, SCLK                      wpi 12 - 14
+  14, 15,                               // UART - Tx, Rx                                wpi 15 - 16
+  28, 29, 30, 31,                       // Rev 2: New GPIOs 8 though 11                 wpi 17 - 20
+   5,  6, 13, 19, 26,                   // B+                                           wpi 21, 22, 23, 24, 25
+  12, 16, 20, 21,                       // B+                                           wpi 26, 27, 28, 29
+   0,  1,                               // B+                                           wpi 30, 31
 
-    -1, -1, /* ... 31 */
-    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, /* . 47 */
-    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, /* . 63 */
-};
+// Padding:
 
-/*
- gpioToGPFSEL:
-      Map a BCM_GPIO pin to it's control port. (GPFSEL 0-5)
- */
-static const uint8_t gpioToGPFSEL [] = {
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-    2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-    3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
-    4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
-    5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
-};
+  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,       // ... 47
+  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,       // ... 63
+} ;
 
-/*
- gpioToShift
-      Define the shift up for the 3 bits per pin in each GPFSEL port
- */
-static const uint8_t gpioToShift [] = {
-    0, 3, 6, 9, 12, 15, 18, 21, 24, 27,
-    0, 3, 6, 9, 12, 15, 18, 21, 24, 27,
-    0, 3, 6, 9, 12, 15, 18, 21, 24, 27,
-    0, 3, 6, 9, 12, 15, 18, 21, 24, 27,
-    0, 3, 6, 9, 12, 15, 18, 21, 24, 27,
-};
+// physToGpio:
+//      Take a physical pin (1 through 26) and re-map it to the BCM_GPIO pin
+//      Cope for 2 different board revisions here.
+//      Also add in the P5 connector, so the P5 pins are 3,4,5,6, so 53,54,55,56
 
-/*
- gpioToGPSET:
-      (Word) offset to the GPIO Set registers for each GPIO pin
- */
-static const uint8_t gpioToGPSET [] = {
-    7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
-    7, 7, 7, 7, 7, 7, 7, 7,
-    8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
-    8, 8, 8, 8, 8, 8, 8, 8,
-};
+static int *physToGpio ;
 
-/*
- gpioToGPCLR:
-      (Word) offset to the GPIO Clear registers for each GPIO pin
- */
-static const uint8_t gpioToGPCLR [] = {
-    10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10,
-    10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10,
-    11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
-    11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
-};
+static int physToGpioR1 [64] =
+{
+  -1,           // 0
+  -1, -1,       // 1, 2
+   0, -1,
+   1, -1,
+   4, 14,
+  -1, 15,
+  17, 18,
+  21, -1,
+  22, 23,
+  -1, 24,
+  10, -1,
+   9, 25,
+  11,  8,
+  -1,  7,       // 25, 26
 
-/*
- gpioToGPLEV:
-      (Word) offset to the GPIO Input level registers for each GPIO pin
- */
-static const uint8_t gpioToGPLEV [] = {
-    13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13,
-    13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13,
-    14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14,
-    14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14,
-};
+                                              -1, -1, -1, -1, -1,       // ... 31
+  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,       // ... 47
+  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,       // ... 63
+} ;
+
+static int physToGpioR2 [64] =
+{
+  -1,           // 0
+  -1, -1,       // 1, 2
+   2, -1,
+   3, -1,
+   4, 14,
+  -1, 15,
+  17, 18,
+  27, -1,
+  22, 23,
+  -1, 24,
+  10, -1,
+   9, 25,
+  11,  8,
+  -1,  7,       // 25, 26
+
+// B+
+
+   0,  1,
+   5, -1,
+   6, 12,
+  13, -1,
+  19, 16,
+  26, 20,
+  -1, 21,
+
+// the P5 connector on the Rev 2 boards:
+
+  -1, -1,
+  -1, -1,
+  -1, -1,
+  -1, -1,
+  -1, -1,
+  28, 29,
+  30, 31,
+  -1, -1,
+  -1, -1,
+  -1, -1,
+  -1, -1,
+} ;
+
+// gpioToGPFSEL:
+//      Map a BCM_GPIO pin to it's Function Selection
+//      control port. (GPFSEL 0-5)
+//      Groups of 10 - 3 bits per Function - 30 bits per port
+
+static uint8_t gpioToGPFSEL [] =
+{
+  0,0,0,0,0,0,0,0,0,0,
+  1,1,1,1,1,1,1,1,1,1,
+  2,2,2,2,2,2,2,2,2,2,
+  3,3,3,3,3,3,3,3,3,3,
+  4,4,4,4,4,4,4,4,4,4,
+  5,5,5,5,5,5,5,5,5,5,
+} ;
+
+
+// gpioToShift
+//      Define the shift up for the 3 bits per pin in each GPFSEL port
+
+static uint8_t gpioToShift [] =
+{
+  0,3,6,9,12,15,18,21,24,27,
+  0,3,6,9,12,15,18,21,24,27,
+  0,3,6,9,12,15,18,21,24,27,
+  0,3,6,9,12,15,18,21,24,27,
+  0,3,6,9,12,15,18,21,24,27,
+} ;
+
+
+// gpioToGPSET:
+//      (Word) offset to the GPIO Set registers for each GPIO pin
+
+static uint8_t gpioToGPSET [] =
+{
+   7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+   8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
+} ;
+
+// gpioToGPCLR:
+//      (Word) offset to the GPIO Clear registers for each GPIO pin
+
+static uint8_t gpioToGPCLR [] =
+{
+  10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,
+  11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,
+} ;
+
+
+// gpioToGPLEV:
+//      (Word) offset to the GPIO Input level registers for each GPIO pin
+
+static uint8_t gpioToGPLEV [] =
+{
+  13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,
+  14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,
+} ;
 
 /*
  * pinMode:
@@ -427,7 +497,6 @@ void pinModeGpio(int pin, int mode) {
 
     fSel = gpioToGPFSEL [pin];
     shift = gpioToShift [pin];
-	return;
 
     /**/ if (mode == INPUT) /* Sets bits to zero = input */
         *(gpio + fSel) = (*(gpio + fSel) & ~(7 << shift));
