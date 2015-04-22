12de437f7190f334e08665101cbff2b7af1b66d9
Fred Brooks <nsaspook@nsaspook.com>
2015-04-21 23:55:07 -0700

    source update

--- daq_gert.c
+++ daq_gert.c
@@ -1075,7 +1075,7 @@ static int daqgert_ai_cmdtest(struct comedi_device *dev,
 	/* step 4: fix up any arguments */
 	if (cmd->convert_src == TRIG_TIMER) {
 		arg = cmd->convert_arg;
-		i8253_cascade_ns_to_timer(10000000,
+		i8253_cascade_ns_to_timer(4000000,
 			&pic_data->divisor1,
 			&pic_data->divisor2,
 			&arg, cmd->flags);
