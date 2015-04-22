619bda544fce724793836781a69667b5fbdc2384
Fred Brooks <nsaspook@nsaspook.com>
2015-04-21 23:09:59 -0700

    source update

--- daq_gert.c
+++ daq_gert.c
@@ -814,8 +814,8 @@ static int daqgert_thread_function(void *data)
 				//				set_current_state(TASK_UNINTERRUPTIBLE);
 				schedule();
 			} else {
-				//				msleep(1);
-				schedule();
+				msleep(1);
+				//schedule();
 			}
 			if (pic_data->timer && pic_data->run) {
 				pic_data->spi_run = true;
@@ -831,7 +831,6 @@ static int daqgert_thread_function(void *data)
 		pic_data->spi_run = false;
 		pic_data->count++;
 		mutex_unlock(&spidata_lock);
-		//		msleep(500);
 		//		dev_info(dev->class_dev, "daq_gert Thread waiting\n");
 	}
 	/*do_exit(1);*/
@@ -986,7 +985,7 @@ static int daqgert_ai_cmd(struct comedi_device *dev, struct comedi_subdevice *s)
 	struct pic_platform_data *pic_data = s->private;
 
 	dev_info(dev->class_dev, "ai_cmd\n");
-	//	if (pic_data->cmd_running) return -EBUSY;
+	if (pic_data->cmd_running) return -EBUSY;
 
 	daqgert_ai_set_chan_range(dev, cmd->chanlist[0], 1);
 	s->async->cur_chan = 0;
@@ -1075,7 +1074,7 @@ static int daqgert_ai_cmdtest(struct comedi_device *dev,
 	/* step 4: fix up any arguments */
 	if (cmd->convert_src == TRIG_TIMER) {
 		arg = cmd->convert_arg;
-		i8253_cascade_ns_to_timer(4000000,
+		i8253_cascade_ns_to_timer(1000000,
 			&pic_data->divisor1,
 			&pic_data->divisor2,
 			&arg, cmd->flags);
