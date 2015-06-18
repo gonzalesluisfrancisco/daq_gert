0026c0853c177220ce265e213c861f8ead5d48cf
Fred Brooks <nsaspook@nsaspook.com>
2015-06-18 08:15:07 -0700

    Source add:  4.1 kernel fixes

--- daq_gert.c
+++ daq_gert.c
@@ -2755,6 +2755,6 @@ module_exit(daqgert_exit);
 
 MODULE_AUTHOR("Fred Brooks <spam@sma2.rain.com>");
 MODULE_DESCRIPTION("RPi DIO/AI/AO Driver");
-MODULE_VERSION("0.0.34");
+MODULE_VERSION("4.1.1");
 MODULE_LICENSE("GPL");
 MODULE_ALIAS("spi:spigert");
